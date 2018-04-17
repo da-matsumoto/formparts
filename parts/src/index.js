f7b38ff9d99c954a546ac2a61fbe5d886769a774
da-matsumoto <da.mats1990@gmail.com>
2018-04-18 00:22:37 +0900

    textboxの使い方

--- parts/src/index.js
+++ parts/src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class TextForm extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            value: ''
+        }
+    }
+        render () {
+            return (<div>
+                <form onSubmit={e => this.doSubmit(e)}>
+                  <input type='text'
+                    onChange={e => this.doChange(e)}
+                    value={this.state.value} />
+                  <input type='submit' />
+                </form>
+            </div>)
+        }
+        doChange (e) {
+            this.setState({ value: e.target.value })
+        }
+        doSubmit (e) {
+            e.preventDefault()
+            window.alert(this.state.value)
+        }
+    }
+    ReactDOM.render(
+        <TextForm />,
+        document.getElementById('root')
+    )
